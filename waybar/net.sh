68c8ee2baa5e937e10884c82774b8b4378126ffd
Cylenia <me@cylenia.dev>
2025-10-29 15:21:41 +0000

    [waybar] fix: misc fixes

--- waybar/net.sh
+++ waybar/net.sh
@@ -1,4 +1,4 @@
-readbytes=$(ifstat --json | jq ".kernel.eth0.rx_bytes")
-writebytes=$(ifstat --json | jq ".kernel.eth0.tx_bytes")
-echo "$(($readbytes/1024)) MiB ↓   $(($writebytes/1024)) MiB ↑"
+readbytes=$(ifstat --json | jq ".kernel.$(ip route get 8.8.8.8 | awk '{print $5}' | xargs).rx_bytes")
+writebytes=$(ifstat --json | jq ".kernel.$(ip route get 8.8.8.8 | awk '{print $5}' | xargs).tx_bytes")
+echo "$(($readbytes/1024)) KiB  | $(($writebytes/1024)) KiB "
 
